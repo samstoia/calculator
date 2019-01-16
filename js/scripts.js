dd23aaacd0824e01a92d0fd698c404dcf0547159
sam stoia and tristan setha <sam@>
2019-01-16 13:26:21 -0800

    It will add user interface to calculator to perform add function using JS and jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
-var add = function(number1, number2){
-	return number1 + number2;
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+
+//everything below this is user interface (front end) logic
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+    event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var addresult = add(number1, number2);
+		$("#output").text(addresult);
+	});
+});
