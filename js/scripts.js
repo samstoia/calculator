004bd2be762b1a2713162b670e774b49a7c3c56b
sam stoia and tristan setha <tristan@>
2019-01-16 14:20:27 -0800

    It will add subtract, multiply, divide forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 // Business (or back-end) logic:
 
 var add = function(number1, number2) {
-  return number1 + number2;
+  return (number1 + number2).toPrecision(4);
 };
 
 var subtract = function(number1, number2) {
-  return number1 - number2;
+  return (number1 - number2).toPrecision(4);
 };
 
 var multiply = function(number1, number2) {
-  return number1 * number2;
+  return (number1 * number2).toPrecision(4);
 };
 
 var divide = function(number1, number2) {
-  return number1 / number2;
+  return (number1 / number2).toPrecision(4);
 };
 
 
@@ -24,6 +24,27 @@ $(document).ready(function() {
 		var number1 = parseInt($("#add1").val());
 		var number2 = parseInt($("#add2").val());
 		var addresult = add(number1, number2);
-		$("#output").text(addresult);
+		$("#addOutput").text(addresult);
+	});
+	$("form#subtract").submit(function(event) {
+    event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var subtractResult = subtract(number1, number2);
+		$("#subtractOutput").text(subtractResult);
+	});
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var multiplyResult = multiply(number1, number2);
+		$("#multiplyOutput").text(multiplyResult);
+	});
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var divideResult = divide(number1, number2);
+		$("#divideOutput").text(divideResult);
 	});
 });
